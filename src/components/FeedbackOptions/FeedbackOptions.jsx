08fa72b569c7a1cece98c3a94d40b89124250049
Serhii-Nefedov <uvaafestival@gmail.com>
2021-07-25 14:23:05 +0300

    add styles for statistics

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,24 @@
+
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return options.map(option => (
+    <button
+      type="button"
+      className={styles.button}
+      key={option}
+      onClick={() => onLeaveFeedback(option)}
+    >
+      {option}
+    </button>
+  ));
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
